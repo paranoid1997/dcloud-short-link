6d8118dccb4103a7f07e422b37178a0d83e08d22
刘先生 <67285173+paranoid1997@users.noreply.github.com>
2023-03-21 22:08:48 +0800

    Update TimeUtil.java

--- dcloud-common/src/main/java/net/xdclass/util/TimeUtil.java
+++ dcloud-common/src/main/java/net/xdclass/util/TimeUtil.java
@@ -3,33 +3,25 @@ package net.xdclass.util;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 /**
- * @author 刘森飚
- * @since 2023-02-03
- */
+ * @Description
+ * @Author 刘森飚
+ **/
 
 public class TimeUtil {
 
+    /**
+     * 默认日期格式
+     */
+    private static final String DEFAULT_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
     /**
      * 默认日期格式
      */
-    private static final String DEFAULT_PATTERN = "yyyy-MM-dd";
-
-    private static final String DEFAULT_PATTERN_WITH_TIME = "yyyy-MM-dd HH:mm:ss";
-
-    /**
-     * 默认日期格式
-     */
-    private static final DateTimeFormatter DEFAULT_DATE_FORMATTER  = DateTimeFormatter.ofPattern(DEFAULT_PATTERN);
-
-    /**
-     * 默认日期时间格式
-     */
-    private static final DateTimeFormatter DEFAULT_DATE_TIME_FORMATTER  = DateTimeFormatter.ofPattern(DEFAULT_PATTERN_WITH_TIME);
-
+    private static final DateTimeFormatter DEFAULT_DATE_TIME_FORMATTER  = DateTimeFormatter.ofPattern(DEFAULT_PATTERN);
 
     private static final ZoneId DEFAULT_ZONE_ID = ZoneId.systemDefault();
 
@@ -66,30 +58,17 @@ public class TimeUtil {
      */
     public static String format(Date time){
 
-        String timeStr = DEFAULT_DATE_FORMATTER.format(time.toInstant().atZone(DEFAULT_ZONE_ID));
+        String timeStr = DEFAULT_DATE_TIME_FORMATTER.format(time.toInstant().atZone(DEFAULT_ZONE_ID));
         return timeStr;
     }
 
     /**
-     *  Date 转 字符串，默认日期格式
-     * @param time
-     * @return
-     */
-    public static String format(Long timestamp){
-
-        String timeStr = DEFAULT_DATE_FORMATTER.format(new Date(timestamp).toInstant().atZone(DEFAULT_ZONE_ID));
-        return timeStr;
-    }
-
-
-
-    /**
-     * timestamp 转 字符串，指定日期格式
+     * timestamp 转 字符串，默认日期格式
      *
      * @param time
      * @return
      */
-    public static String formatWithTime(long timestamp) {
+    public static String format(long timestamp) {
         String timeStr = DEFAULT_DATE_TIME_FORMATTER.format(new Date(timestamp).toInstant().atZone(DEFAULT_ZONE_ID));
         return timeStr;
     }
@@ -102,8 +81,25 @@ public class TimeUtil {
      * @return
      */
     public static Date strToDate(String time) {
-        LocalDateTime localDateTime = LocalDateTime.parse(time, DEFAULT_DATE_FORMATTER);
+        LocalDateTime localDateTime = LocalDateTime.parse(time, DEFAULT_DATE_TIME_FORMATTER);
         return Date.from(localDateTime.atZone(DEFAULT_ZONE_ID).toInstant());
 
+    }
+
+
+    /**
+     * 获取当天剩余的秒数,用于流量包过期配置
+     * @param currentDate
+     * @return
+     */
+    public static Integer getRemainSecondsOneDay(Date currentDate) {
+        LocalDateTime midnight = LocalDateTime.ofInstant(currentDate.toInstant(),
+                ZoneId.systemDefault()).plusDays(1).withHour(0).withMinute(0)
+                .withSecond(0).withNano(0);
+
+        LocalDateTime currentDateTime = LocalDateTime.ofInstant(currentDate.toInstant(),
+                ZoneId.systemDefault());
+        long seconds = ChronoUnit.SECONDS.between(currentDateTime, midnight);
+        return (int) seconds;
     }
 }
